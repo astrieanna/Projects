0b06735295ffe56141940da73e5728931a7637c6
Leah Hanson <astrieanna@gmail.com>
2013-07-25 17:20:01 -0400

    replaced factorial.py with factorial.jl

---
+++ Numbers/factorial.jl
@@ -0,0 +1,28 @@
+#Factorial Finder - The Factorial of a positive integer, n,
+#is defined as the product of the sequence n, n-1, n-2, ...1
+#and the factorial of zero, 0, is defined as being 1. Solve
+#this using both loops and recursion.
+
+function fact_loop(n)
+    fact = 1
+    while n > 0
+        fact *= n
+        n -= 1
+    end
+    return fact
+end
+
+function fact_recursion(n)
+    return n == 0 ? 1 : n * fact_recursion(n - 1) 
+end
+
+print("Please enter a positive number: ")
+n = BigInt(int(readline(STDIN)))
+
+if n >= 0
+   println("Factorial of $n by loops is $(fact_loop(n))")
+   println("Factorial of $n by recursion is $(fact_recursion(n))")
+   println("Factorial of $n by built-in is $(factorial(n))")
+else
+    println("Not a valid number")
+end
