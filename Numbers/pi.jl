29ad8d9157f27ddd349a47992ad7cb8a25562f03
Leah Hanson <astrieanna@gmail.com>
2013-07-25 18:43:37 -0400

    made pi.jl take a commandline arg

--- Numbers/pi.jl
+++ Numbers/pi.jl
@@ -1,8 +1,15 @@
 # Find pi to the nth digit
 
-## Get n from user input (STDIN)
-print("Please enter an integer: ")
-n = int(readline(STDIN))
+n = 0
+if length(ARGS) == 0
+  ## Get n from user input (STDIN)
+  print("Please enter an integer: ")
+  n = int(readline(STDIN))
+elseif length(ARGS) == 1
+  n = int(ARGS[1])
+else
+  error("Invalid number of arguments. Either provide one or zero integers.")
+end
 
 ## Find pi to nth digit
 short_pi = round(pi,n-1) #doesn't work for n > 15
