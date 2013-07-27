396d17cc2e1aed61817d7c702fc718e0bbe1e2e7
Leah Hanson <astrieanna@gmail.com>
2013-07-27 12:52:22 -0400

    replaced calc.py with calc.jl

---
+++ Numbers/calc.jl
@@ -0,0 +1,37 @@
+function request_float()
+  print("Enter a number: ")
+  float(readline(STDIN))
+end
+
+function op2function(op::String)
+  op == "+" ? (+) :
+  op == "-" ? (-) :
+  op == "*" ? (*) :
+  op == "/" ? (/) :
+  op == "%" ? (%) :
+  error("Invalid operator $op")
+end  
+
+function calculate(n1)
+  println("$n1 _ _ = _")
+  
+  print("Enter an operator (+,-,*,/,%): ")
+  opstr= strip(readline(STDIN))
+  op = op2function(opstr)
+  println("$n1 $opstr _ = _")
+
+  n2 = request_float()
+  n3 = op(n1,n2)
+  println("$n1 $opstr $n2 = $n3\n")
+  n3
+end
+
+n1 = request_float()
+
+while true
+  n1 = calculate(n1)
+
+  print("Enter '0' to exit: ")
+  resp = strip(readline(STDIN))
+  if resp == "0" return end
+end
