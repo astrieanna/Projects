6ea8327276e54a652cdc94f9ccc7a894c4f11fa1
Leah Hanson <astrieanna@gmail.com>
2013-07-31 19:05:33 -0400

    piglatin now handles punctuation at the ends of words

--- Text/piglatin.jl
+++ Text/piglatin.jl
@@ -9,13 +9,30 @@ s = ArgParseSettings()
   help = "a file of text to translate to pig latin"
   arg_type = String
 end
+
+function piglatin(word::String)
+  words = split(word,"-")
+  out = String[] 
+  for word1 in words
+    w = collect(word1) #Vector{Char}
+    ending = Char[] 
+    while contains(".,;:!?#&",w[end])
+      push!(ending,pop!(w))
+    end
+    ps = join(ending)
+    push!(out, "$(join(w[2:end]))$(w[1])ay$ps")
+  end
+  join(out,"-")
+end
+
 args = parse_args(s)
 println(args)
 str = args["word"] != nothing ? args["word"] :
-      args["file"] != nothing ? args["file"] :
+      args["file"] != nothing ? readall(open(args["file"])) :
       begin
         println(ArgParse.usage_string(s)) 
         error("please provide a word or a file (see --help)")
       end
-
-println("$(str[2:end])$(str[1])ay") 
+words = split(lowercase(strip(str)))
+words = map(piglatin,words)
+println(join(words," ")) 
